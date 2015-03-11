9d6584f2619a90ceab5074d9ea9e5f8132d8064f
Alexander Semchenkov <Azzoo@users.noreply.github.com>
2015-03-11 23:48:30 +0200

    Update custom_type.js

--- examples/custom_type.js
+++ examples/custom_type.js
@@ -5,20 +5,29 @@ var fortune = require('../lib/fortune');
 var app = fortune({
 db: 'petstore'
 })
-.customType('price', {
-amount: Number,
-units: String
+.customType('price', { //Define 'price' custom type. It's have no instance, only used in metadata.
+  amount: Number,
+  units: String
 })
 .resource('person', {
-name: String,
-age: Number,
-pets: ['pet'] // "has many" relationship to pets
+  name: String,
+  age: Number,
+  pets: ['pet'] // "has many" relationship to pets
 })
 .resource('pet', {
-name: String,
-age: Number,
-owner: 'person', // "belongs to" relationship to a person
-cost: 'price'
+  name: String,
+  age: Number,
+  owner: 'person', // "belongs to" relationship to a person
+  cost: 'price' //Using previous defined custom type 'price'
+  /*  cost: 'price'
+   *   
+   *  is equal to this:
+   *
+   *  cost : {
+   *    amount: Number,
+   *    units: String
+   *  }
+   */
 })
 .listen(1337);//Start the API
 
