7d132be6ba04504e07a090e2f21a5c9388d715fe
Alexander Semchenkov <Azzoo@users.noreply.github.com>
2015-03-16 16:11:12 +0200

    Create custom-types.js

---
+++ test/fortune/custom-types.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Alexander Semchenkov on 16.03.2015.
+ */
+'use strict';
+var should = require('should');
+var request = require('supertest');
+
+module.exports = function(options){
+  describe('custom types', function(){
+    var baseUrl;
+    beforeEach(function(){
+      baseUrl = options.baseUrl;
+    });
+    it('checking custom type defined correctly', function(done){
+      request(baseUrl).post('/car')
+        .set('content-type', 'application/vnd.api+json')
+        .send(JSON.stringify({
+          licenseNumber: "GHJ353",
+          model: "TestCarWithCustomPrice",
+          additionalDetails: {
+            seats: 2,
+            price: {
+              value : 15550.20,
+              currency : "USD"
+            }
+          }
+        }))
+        .expect(200)
+        .end(function(err, res){
+          should.not.exist(err);
+          res.additionalDetails.price.should.equal({
+            value : 15550.20,
+            currency : "USD"
+          });
+        });
+      done();
+    });
+  });
+};
+ No newline at end of file
