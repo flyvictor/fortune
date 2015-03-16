92796d19e8513b8931fe3bc08386e9dde579ff14
Alexander Semchenkov <Azzoo@users.noreply.github.com>
2015-03-16 21:40:43 +0200

    Update custom-types.js

--- test/fortune/custom-types.js
+++ test/fortune/custom-types.js
@@ -1,5 +1,5 @@
 /**
- * Created by Alexander Semchenkov on 16.03.2015.
+ * Created by Mirror on 16.03.2015.
  */
 'use strict';
 var should = require('should');
@@ -12,29 +12,34 @@ module.exports = function(options){
       baseUrl = options.baseUrl;
     });
     it('checking custom type defined correctly', function(done){
-      request(baseUrl).post('/car')
-        .set('content-type', 'application/vnd.api+json')
+      request(baseUrl).post('/cars')
+        .set('content-type', 'application/json')
         .send(JSON.stringify({
-          licenseNumber: "GHJ353",
-          model: "TestCarWithCustomPrice",
-          additionalDetails: {
-            seats: 2,
-            price: {
-              value : 15550.20,
-              currency : "USD"
-            }
-          }
+          cars : [{
+              //Generates unique license number
+              licenseNumber: "GG" + (Math.random() + "").replace('0.', ''),
+              model: "TestCarWithCustomPrice",
+              additionalDetails: {
+                seats: 2,
+                price: {
+                  value : 15550.20,
+                  currency : "USD"
+                }
+              }
+          }]
         }))
-        .expect(200)
+        .expect(201)
         .end(function(err, res){
           should.not.exist(err);
-          res.additionalDetails.price.should.equal({
-            value : 15550.20,
-            currency : "USD"
-          });
+          var body = JSON.parse(res.text);
+          var car = body.cars[0];
+          should.exist(car.additionalDetails.price);
+          should.exist(car.additionalDetails.price.currency);
+          should.exist(car.additionalDetails.price.value);
+          car.additionalDetails.price.currency.should.equal("USD");
+          car.additionalDetails.price.value.should.equal(15550.20);
+          done();
         });
-      done();
     });
   });
 };
-
